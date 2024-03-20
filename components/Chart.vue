626a1e65df68a815e1c84936800d730449b3b2f6
21abdilah <158019652+21abdilah@users.noreply.github.com>
2024-03-20 09:43:17 +0700

    Add files via upload

--- components/Chart.vue
+++ components/Chart.vue
@@ -1,42 +1,33 @@
 <script setup lang="ts">
-import  Chart from 'Chart.js/auto';
+import Chart from "Chart.js/auto";
 
-const labels = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'june',
-];
+const labels = ["January", "February", "March", "April", "May", "june"];
 
 const data = {
-    labels: labels,
-    datasets: [{
-        label: 'My First datasets',
-        backgroundColor: 'rgb(225, 99, 132)',
-        borderColor: 'rgb(225, 99, 132)',
-        data: [15, 10, 5, 2, 20, 30, 45],
-    }]
-
+  labels: labels,
+  datasets: [
+    {
+      label: "My First datasets",
+      backgroundColor: "rgb(225, 99, 132)",
+      borderColor: "rgb(225, 99, 132)",
+      data: [15, 10, 5, 2, 20, 30, 45],
+    },
+  ],
 };
 
 const config = {
-    type: 'bar',
-    data: data,
-    options: {}
+  type: "bar",
+  data: data,
+  options: {},
 };
 
-onUnmounted(() => {
-    const myChart = new Chart(
-        document.getElementById('myChart'),
-        config
-    );
-})
+onMounted(() => {
+  const myChart = new Chart(document.getElementById("myChart"), config);
+});
 </script>
 
 <template>
-    <div>
-        <canvas id="myChart"></canvas>
-    </div>
+  <div>
+    <canvas id="myChart"></canvas>
+  </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,70 +1,79 @@
 <template>
-    <div class="container-fluid">
-      <div class="row my-5">
-        <div class="col-lg-6">
-            <nuxt-link to="/pengunjung/tambah">
-                <div class="card bg-pengunjung rounded-5">
-                    <div class="cardbody">
-                        <h2>Pengunjung</h2>
-                    </div>
-                </div>
-              </nuxt-link>
-          </div>
-         
-            <div class="col-lg-6">
-                <nuxt-link to="/buku">
-                    <div class="card bg-buku rounded-5">
-                        <div class="card-body">
-                        <h2>Cari Buku</h2>
-                    </div>
-                </div>
-                </nuxt-link>
+  <div class="container-fluid">
+    <div class="row my-5">
+      <div class="col-lg-6">
+        <nuxt-link to="/pengunjung/tambah">
+          <div class="card bg-pengunjung rounded-5">
+            <div class="cardbody">
+              <h2>Pengunjung</h2>
             </div>
-        </div>
-        <div class="statistik">
-            <h1>STATISTIK</h1>
-        </div>
-        <div class="row my-5">
-        <div class="col-lg-6">
-            <nuxt-link to="/pengunjung/tambah">
-                <div class="card bg-light rounded-5">
-                    <div class="cardbody">
-                        <h2></h2>
-                    </div>
-                </div>
-              </nuxt-link>
           </div>
-         
-            <div class="col-lg-6">
-                <nuxt-link to="/buku">
-                    <div class="card bg-light rounded-5">
-                        <div class="card-body">
-                        <h2></h2>
-                    </div>
-                </div>
-                </nuxt-link>
+        </nuxt-link>
+      </div>
+
+      <div class="col-lg-6">
+        <nuxt-link to="/buku">
+          <div class="card bg-buku rounded-5">
+            <div class="card-body">
+              <h2>Cari Buku</h2>
             </div>
-        </div>
+          </div>
+        </nuxt-link>
+      </div>
     </div>
-    <div>
-        <Chart />
+    <div class="statistik">
+      <h1>STATISTIK</h1>
     </div>
+    <div class="row my-5">
+      <div class="col-lg-6">
+        <nuxt-link to="/pengunjung/tambah">
+          <div class="card bg-light rounded-5">
+            <div class="cardbody">
+              <h2>4</h2>
+              <h2 class="pt-5 ps-5">Pengunjung</h2>
+            </div>
+          </div>
+        </nuxt-link>
+      </div>
+
+      <div class="col-lg-6">
+        <nuxt-link to="/buku">
+          <div class="card bg-light rounded-5">
+            <div class="card-body text">
+              <h2>4</h2>
+              <h2 class="pt-5 ps-5">Pengunjung</h2>
+            </div>
+          </div>
+        </nuxt-link>
+      </div>
+    </div>
+  </div>
+  <div>
+    <Chart />
+  </div>
 </template>
 
 <style scoped>
 .card {
-    height: 250px;
-    box-shadow: 1px 1px 10px #424242;
+  height: 250px;
+  box-shadow: 1px 1px 10px #424242;
+  margin-top: 15px;
 }
 .card.bg-pengunjung {
-    background-image:  url('../assets/img/bg-home-kunjungan.jpeg');
-    background-repeat: no-repeat;
-    background-position: center center;
-    background-size: cover;
+  background-image: url("../assets/img/bg-home-kunjungan.jpeg");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
 }
 .card.bg-buku {
-    background: url('../assets/img/bg-home-cari-buku.jpg') no-repeat center center;
-    background-size: cover;
+  background: url("../assets/img/bg-home-cari-buku.jpg") no-repeat center center;
+  background-size: cover;
 }
-
+.text {
+  display: flex;
+  align-items: center;
+}
+.text > h1 {
+  font-size: 7rem;
+}
 </style>
